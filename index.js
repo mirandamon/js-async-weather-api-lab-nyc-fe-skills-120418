167c0e8dc4e1c132972055e66811a60544e64da2
Nathan Miranda <nate845@gmail.com>
2019-02-07 23:34:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const API_KEY = "YOUR API KEY"
 
 // 1. We need an event listener for the input box ('submit')
-// 2. 
+// 2. Fetches data from the weather API 
 
 
 function handleFormSubmit(event) {
