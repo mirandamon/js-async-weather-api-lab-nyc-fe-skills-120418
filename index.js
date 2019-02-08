ee0f182d4181577d5917384b93c8953c4a93a640
Nathan Miranda <nate845@gmail.com>
2019-02-08 00:43:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,9 +49,8 @@ function fetchFiveDayForecast(city) {
 
 function displayFiveDayForecast(json) {
   //render five day forecast data to the DOM using provided IDs and json from API
-  console.log(json)
   for (let forecast of json.list) {
-    
+    console.log(forecast.main.temp_min)
   }
 }
 
