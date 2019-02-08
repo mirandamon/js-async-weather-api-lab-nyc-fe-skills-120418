302eda59db610eec2bf937df20e180c746ab2490
Nathan Miranda <nate845@gmail.com>
2019-02-08 00:09:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function handleFormSubmit(event) {
 function fetchCurrentWeather(city) {
   //fetch current weather based on city
   // query parameter
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city)
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=' + API_KEY)
 }
 
 function displayCurrentWeather(json) {
